6e1dbf8b172feb83a2777279c1c17e419d0f7fcb
Annetteskou <Eaa24amsk@students.eaaa.dk>
2025-09-03 10:52:13 +0200

    z

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Navigation from "./component/Navigation";
+
 
 function App() {
   return (
+    <div>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 
 import ReactDOM from "react-dom/client";
+import React from "react";
 import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
-import "./index.css";
+import "./main.css";
 import { StrictMode } from "react";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );

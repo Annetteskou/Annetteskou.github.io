e2b782e4117de1994993994e72cae631796ec926
Annetteskou <Eaa24amsk@students.eaaa.dk>
2025-09-03 09:24:26 +0200

    pages

---
+++ Annettes Portfolie/pages/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <main>
+      <CVHero />
+      <section>
+        <h2>Om mig</h2>
+        <p>
+          Her kan du skrive lidt om dig selv, dine erfaringer og kompetencer.
+        </p>
+      </section>
+    </main>
+  );
+}
---
+++ Annettes Portfolie/src/main.jsx
@@ -0,0 +1,15 @@
+// main.jsx or App.jsximport React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter
+      basename={import.meta.env.DEV ? "/" : "/Annetteskou.github.io/"}
+    >
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);

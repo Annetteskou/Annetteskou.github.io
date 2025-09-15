d4cad09207e7826a9a40ef004289b25a2c1309f0
Annetteskou <Eaa24amsk@students.eaaa.dk>
2025-09-15 14:09:41 +0200

    forside

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,9 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <main className={styles.home}>
-      {/* PILL NAV øverst */}
+      {/* Top pill-nav */}
       <nav className={styles.pillNav}>
         <span className={`${styles.pill} ${styles.pillCode}`}>&lt;/&gt;</span>
-
         <NavLink
           to="/"
           end
@@ -17,7 +16,6 @@ export default function Home() {
         >
           Hjem
         </NavLink>
-
         <NavLink
           to="/project"
           className={({ isActive }) =>
@@ -26,7 +24,6 @@ export default function Home() {
         >
           Projekt
         </NavLink>
-
         <NavLink
           to="/about"
           className={({ isActive }) =>
@@ -35,7 +32,6 @@ export default function Home() {
         >
           Om mig
         </NavLink>
-
         <NavLink
           to="/contact"
           className={({ isActive }) =>
@@ -46,13 +42,13 @@ export default function Home() {
         </NavLink>
       </nav>
 
-      {/* Øverste tykke linje */}
+      {/* Øverste linje (kant-til-kant) */}
       <div className={`${styles.rule} ${styles.ruleTop}`} />
 
-      {/* HERO */}
+      {/* HERO GRID */}
       <section className={styles.hero}>
-        {/* Venstre kolonne */}
-        <div>
+        {/* Række 1: Titel (venstre) + Rolle-tekst (højre) */}
+        <div className={styles.titleBlock}>
           <h1 className={styles.title}>
             Velkommen
             <br />
@@ -60,55 +56,73 @@ export default function Home() {
             <br />
             portfolio
           </h1>
-          
-          {/* Mellemlinje */}
-          <div className={`${styles.rule} ${styles.ruleMid}`} />
+        </div>
 
-          <div className={styles.helloRow}>
-            <span className={styles.muted}>...Hej jeg er</span>
-            <span className={styles.dash} />
-            <span className={styles.name}>
-              Annette
-              <br />
-              Skou
-            </span>
+        <p className={styles.role}>
+          <span className={styles.chev}>&gt;</span> Multimediedesigner Studterne
+          <br />
+          <span className={styles.chev}>&gt;</span>{" "}
+          <span className={styles.subRole}>& Fronted developer studterne</span>
+        </p>
+
+        {/* Række 2: Midterlinje på tværs */}
+        <div className={`${styles.rule} ${styles.ruleMid}`} />
+
+        {/* Række 3: Intro + socials (venstre) + Monitor (højre) */}
+        <div className={styles.leftBottom}>
+          <div className={styles.introSection}>
+            <div className={styles.helloRow}>
+              <span className={styles.muted}>...Hej jeg er</span>
+              <span className={styles.dash} />
+              <span className={styles.name}>
+                Annette
+                <br />
+                Skou
+              </span>
+            </div>
+            <div className={styles.codeSymbol}>
+              &lt;/&gt;
+            </div>
           </div>
 
           <div className={styles.socials}>
             <a href="https://github.com/" target="_blank" rel="noreferrer">
-              <img src="/GITHUB.png" alt="GitHub" />
+              <img
+                src="/GITHUB.png"
+                alt="GitHub"
+                className={styles.githubIcon}
+              />
             </a>
             <a href="https://linkedin.com/" target="_blank" rel="noreferrer">
-              <img src="/LINK.png" alt="LinkedIn" />
+              <img
+                src="/LINK.png"
+                alt="LinkedIn"
+                className={styles.linkedinIcon}
+              />
             </a>
             <a href="https://instagram.com/" target="_blank" rel="noreferrer">
-              <img src="/instagram.png" alt="Instagram" />
+              <img
+                src="/instagram.png"
+                alt="Instagram"
+                className={styles.instagramIcon}
+              />
             </a>
             <a href="https://facebook.com/" target="_blank" rel="noreferrer">
-              <img src="/facebook.png" alt="Facebook" />
+              <img
+                src="/facebook.png"
+                alt="Facebook"
+                className={styles.facebookIcon}
+              />
             </a>
           </div>
         </div>
 
-        {/* Højre kolonne */}
-        <div className={styles.right}>
-          <p className={styles.role}>
-            <span className={styles.chev}>&gt;</span>
-            Multimediedesigner studerende
-            <br />
-            <span className={styles.chev}>&gt;</span>
-            <span className={styles.subRole}>
-              Frontend-udvikler (studerende)
-            </span>
-          </p>
-
-          <div className={styles.monitorWrap}>
-            <div className={styles.monitorFrame}>
-              <img
-                src="/Computer Coding Sticker by Rijn IJssel.gif"
-                alt="Coding monitor"
-              />
-            </div>
+        <div className={styles.monitorWrap}>
+          <div className={styles.monitorFrame}>
+            <img
+              src="/Computer Coding Sticker by Rijn IJssel.gif"
+              alt="Coding monitor"
+            />
           </div>
         </div>
       </section>
